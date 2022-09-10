b5051f8d99bc9ae2409fe1471a48d65fcbfeb8ea
sotair <krzys.sota@gmail.com>
2022-09-10 18:25:55 +0200

    measure time io/cpu

--- db_client.py
+++ db_client.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Optional
-
+import time
 import aerospike
 from snappy import snappy
 
@@ -31,6 +31,7 @@ class MyAerospikeClient:
 
     def delete_all_records(self):
         self.client.truncate(self.namespace, self.set, 0)
+
     def log_all_records(self):
         def print_result(record_tuple):
             key, metadata, record = record_tuple
@@ -52,46 +53,68 @@ class MyAerospikeClient:
             return False
 
     def add_tag(self, user_tag: UserTag):
+        for i in range(3):
+            key = (self.namespace, self.set, user_tag.cookie)
+            user_profile = self.get_user_profile(key, user_tag.cookie, i)
+            if not user_profile:
+                user_profile = UserProfile.parse_obj({"cookie": user_tag.cookie, "buys": [], "views": []})
 
-        user_profile = self.get_user_profile(user_tag.cookie)
-        if not user_profile:
-            user_profile = UserProfile.parse_obj({"cookie": user_tag.cookie, "buys": [], "views": []})
+            if user_tag.action == Action.VIEW:
+                if len(user_profile.views) == MAX_TAG_NUMBER:
+                    user_profile.views.pop(0)
+                user_profile.views.append(user_tag)
+            else:
+                if len(user_profile.buys) == MAX_TAG_NUMBER:
+                    user_profile.buys.pop(0)
+                user_profile.buys.append(user_tag)
 
-        if user_tag.action == Action.VIEW:
-            if len(user_profile.views) == MAX_TAG_NUMBER:
-                user_profile.views.pop(0)
-            user_profile.views.append(user_tag)
-        else:
-            if len(user_profile.buys) == MAX_TAG_NUMBER:
-                user_profile.buys.pop(0)
-            user_profile.buys.append(user_tag)
+            t2 = time.time()
+            print(f"get_user_profile took {t1 - t0}s")
+            success = self.put_user_profile(key, user_profile, i)
+            t3 = time.time()
 
-        self.put_user_profile(user_profile)
+            if success:
+                return
 
-    def get_user_profile(self, cookie: str) -> Optional[UserProfile]:
+    def get_user_profile(self, key: (str, str, str), cookie: str, i: int) -> Optional[UserProfile]:
         try:
-            key = (self.namespace, self.set, cookie)
+            t0 = time.time()
             (key, meta, bins_json) = self.client.get(key)
+            t1 = time.time()
+            print(f"get_user_profile({i}) client.get took {t1 - t0}s")
             ser_bs = snappy.decompress(bins_json["buys"]).decode("utf-8")
             ser_vs = snappy.decompress(bins_json["views"]).decode("utf-8")
             bs = deserialize_tags(ser_bs)
             vs = deserialize_tags(ser_vs)
-            return UserProfile.parse_obj({"cookie": cookie, "buys": bs, "views": vs})
+            res = UserProfile.parse_obj({"cookie": cookie, "buys": bs, "views": vs})
+            t2 = time.time()
+            print(f"get_user_profile({i}) computation took {t2 - t1}s")
+            return res
         except aerospike.exception.RecordNotFound:
             return None  # new user
         except aerospike.exception.AerospikeError as e:
             print(f"error reading user_profile(%s) %s", cookie, e)
 
-    def put_user_profile(self, user_profile: UserProfile):
+    def put_user_profile(self, key: (str, str, str), user_profile: UserProfile, i: int) -> bool:
         try:
-            key = (self.namespace, self.set, user_profile.cookie)
+            t0 = time.time()
             ser_bs = serialize_tags(user_profile.buys)
             ser_vs = serialize_tags(user_profile.views)
             comp_bs = snappy.compress(ser_bs)
             comp_vs = snappy.compress(ser_vs)
-            self.client.put(key, {"cookie": user_profile.cookie, "buys": comp_bs, "views": comp_vs})
+            t1 = time.time()
+            print(f"put_user_profile({i}) computation took {t1 - t0}s")
+            self.client.put(key, {"cookie": user_profile.cookie, "buys": comp_bs, "views": comp_vs},
+                            policy={"gen": aerospike.POLICY_GEN_EQ})
+            t2 = time.time()
+            print(f"put_user_profile({i}) client.put took {t2 - t1}s")
+            return True
+        except aerospike.exception.RecordGenerationError:
+            print(f"{i + 1}. generation error while trying to write user profile for: {user_profile.cookie}")
+            return False
         except aerospike.exception.AerospikeError as e:
             print(f"error writing user_profile(%s) %s", user_profile.cookie, e)
+            return False
 
     def close(self):
         self.client.close()
