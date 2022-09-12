a28dfc66cc4bfa684f800605af3cacdf321b7377
sotair <krzys.sota@gmail.com>
2022-09-12 21:36:16 +0200

    logging debug

--- db_client.py
+++ db_client.py
@@ -64,6 +64,8 @@ class MyAerospikeClient:
             self.logger.error("key to delete not found: %s", key)
             return False
     def add_tag(self, user_tag: UserTag) -> bool:
+        print(f"halko?")
+
         for i in range(10):
             (user_profile, gen) = self.get_user_profile(user_tag.cookie, i)
             if not user_profile:
@@ -75,7 +77,7 @@ class MyAerospikeClient:
                 user_profile.views.append(user_tag)
                 sorted_vs = sorted(user_profile.views, key=lambda t: t.time)
                 if sorted_vs != user_profile.views:
-                    print(f"{user_tag.cookie} views not sorted {(user_profile)}")
+                    #print(f"{user_tag.cookie} views not sorted {(user_profile)}")
                     user_profile.views = sorted_vs
             else:
                 if len(user_profile.buys) == MAX_TAG_NUMBER:
@@ -83,7 +85,7 @@ class MyAerospikeClient:
                 user_profile.buys.append(user_tag)
                 sorted_bs = sorted(user_profile.buys, key=lambda t: t.time)
                 if sorted_bs != user_profile.buys:
-                    print(f"{user_tag.cookie} buys not sorted {(user_profile)}")
+                    #print(f"{user_tag.cookie} buys not sorted {(user_profile)}")
                     user_profile.buys = sorted_bs
             if self.put_user_profile(user_profile, gen, i):
                 return True
