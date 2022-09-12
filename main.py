d9c3442bf53cda51451e320415c54cc3497a6d8b
sotair <krzys.sota@gmail.com>
2022-09-12 21:26:20 +0200

    10 workers

--- main.py
+++ main.py
@@ -101,8 +101,7 @@ def sync_user_profile(cookie: str, time_range: str, user_profile_result: Union[U
 @app.post("/user_profiles/{cookie}")
 async def user_profiles(cookie: str, time_range: str, user_profile_result: Union[UserProfile, None] = None,
                         limit: int = 200):
-    return user_profile_result
-    #return await asyncify(sync_user_profile)(cookie, time_range, user_profile_result, limit)
+    return await asyncify(sync_user_profile)(cookie, time_range, user_profile_result, limit)
 
 
 ###        DEBUG ENDPOINTS        ###
