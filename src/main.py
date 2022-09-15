1ad2028aa6c750b929fc622834068585a998644f
sotair <krzys.sota@gmail.com>
2022-09-15 22:52:16 +0200

    20 workers

--- src/main.py
+++ src/main.py
@@ -18,10 +18,6 @@ def shutdown():
 
 
 @app.post("/user_tags")
-def dummy_user_tags(user_tag: UserTag):
-    return Response(status_code=204)
-
-
 def handle_user_tags(user_tag: UserTag):
     if db_client.add_tag(user_tag):
         return Response(status_code=204)
@@ -29,12 +25,12 @@ def handle_user_tags(user_tag: UserTag):
         logger.error(f"{user_tag.cookie} couldn't add tag {user_tag}")
         return Response(status_code=400)
 
+def dummy_user_tags(user_tag: UserTag):
+    return Response(status_code=204)
+
+
 
 @app.post("/user_profiles/{cookie}")
-def dummy_user_profiles(cookie: str, time_range: str, user_profile_result: Union[UserProfile, None] = None,
-                         limit: int = 200):
-    return user_profile_result
-
 def handle_user_profiles(cookie: str, time_range: str, user_profile_result: Union[UserProfile, None] = None,
                          limit: int = 200):
     (user_profile, _) = db_client.get_user_profile(cookie)
@@ -66,6 +62,10 @@ def handle_user_profiles(cookie: str, time_range: str, user_profile_result: Unio
             "views": [],
             "buys": [],
         }
+
+def dummy_user_profiles(cookie: str, time_range: str, user_profile_result: Union[UserProfile, None] = None,
+                         limit: int = 200):
+    return user_profile_result
 
 
 #          DEVELOPMENT        #
